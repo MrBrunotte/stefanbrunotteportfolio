490c901f86eb4ff3c9cb81c63a21efaab86c902c
Stefan Brunotte <mrbrunotte@gmail.com>
2022-10-10 11:56:45 +0200

    Test add extra component for GitHub pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 import MyNavbar from './components/navbar/myNavbar';
 import Landingpage from './components/landingpage/landingpage';
+import StefanBrunottePortfolio from './components/landingpage/landingpage2';
 import About  from './components/about/about';
 import MyProjects from './components/myProjects/myProjects';
 import Work from './components/work/work';
@@ -27,6 +28,7 @@ let App = () => {
 
     <Routes>
       <Route path="/" element={<Landingpage />} />
+      <Route path="/StefanBrunottePortfolio" element={<StefanBrunottePortfolio />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<MyProjects />} />
       <Route path="/work" element={<Work />} />
