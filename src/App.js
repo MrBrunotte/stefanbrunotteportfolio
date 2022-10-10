900ec0f7124439ccd0cb8dce277eb30ae18f7080
Stefan Brunotte <mrbrunotte@gmail.com>
2022-10-10 12:00:54 +0200

    Removed the test files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 import MyNavbar from './components/navbar/myNavbar';
 import Landingpage from './components/landingpage/landingpage';
-import StefanBrunottePortfolio from './components/landingpage/landingpage2';
 import About  from './components/about/about';
 import MyProjects from './components/myProjects/myProjects';
 import Work from './components/work/work';
@@ -28,7 +27,6 @@ let App = () => {
 
     <Routes>
       <Route path="/" element={<Landingpage />} />
-      <Route path="/StefanBrunottePortfolio" element={<StefanBrunottePortfolio />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<MyProjects />} />
       <Route path="/work" element={<Work />} />
