b61a64729684ac90c9ab426efa2fbe1cd021362f
Stefan Brunotte <mrbrunotte@gmail.com>
2022-10-20 10:21:14 +0200

    Added onClick

--- src/components/navbar/myNavbar.js
+++ src/components/navbar/myNavbar.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -5,27 +6,26 @@ import { Link } from 'react-router-dom';
 import './myNavbar.css';
 
 function MyNavbar() {
-    return (
-      <Navbar className='nav-background nav-padding' expand="lg" variant='dark'>
-      <Container fluid>
-        <Navbar.Toggle aria-controls="navbarScroll"/>
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="ms-auto nav-style" style={{ maxHeight: '300px' }} navbarScroll>
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+        <Container fluid>
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto nav-style">
 
-            <Nav.Link className='my-nav-link' as={Link} to="/">Home</Nav.Link>
-            <Nav.Link className='my-nav-link' as={Link} to="/about">About</Nav.Link>
-            <Nav.Link className='my-nav-link' as={Link} to="/projects">My Projects</Nav.Link>
-            <Nav.Link className='my-nav-link' as={Link} to="/work">Work</Nav.Link>
-            <Nav.Link className='my-nav-link' as={Link} to="/academics">Academics</Nav.Link>
-            <Nav.Link className='my-nav-link' as={Link} to="/contact">Contact</Nav.Link>
-            <Nav.Link className='my-nav-link my-resume' as={Link} to="/resume">Resume/CV</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
-        
+              <Nav.Link onClick={() => setExpanded(false)} className="my-nav-link" as={Link} to="/">Home</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link' as={Link} to="/about">About</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link' as={Link} to="/projects">My Projects</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link' as={Link} to="/work">Work</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link' as={Link} to="/academics">Academics</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link' as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link onClick={() => setExpanded(false)} className='my-nav-link my-resume' as={Link} to="/resume">Resume/CV</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>  
     );
   }
 
