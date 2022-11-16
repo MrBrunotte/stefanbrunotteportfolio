4bf6ce5d14d068e6f4eefa15cd5d53e7645f4c81
Stefan Brunotte <mrbrunotte@gmail.com>
2022-11-16 14:13:03 +0100

    Email fix

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import jag from '../../images/jag.png';
-import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
 import './about.css';
 
 let About = () => {
